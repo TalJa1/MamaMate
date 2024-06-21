84ef7316a8a0ae7872f1564c05592345cc643db6
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-06-21 15:33:13 +0700

    add asset

--- pages/QuestionPage.tsx
+++ pages/QuestionPage.tsx
@@ -1,11 +1,22 @@
 /* eslint-disable prettier/prettier */
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions, Image} from 'react-native';
 import React from 'react';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const {width, height} = Dimensions.get('window');
 
 const QuestionPage = () => {
   return (
     <View style={styles.container}>
-      <Text>QuestionPage</Text>
+      <View style={styles.upperview}>
+        <Text style={styles.initText}>
+          Là bố hay là mẹ đang mong chờ sự ra đời của con vậy ạ?
+        </Text>
+      </View>
+      <View style={styles.lowerview}>
+        <Text>H</Text>
+        <Image source={require('../assets/questionsAssets/yoga1.png')} />
+      </View>
     </View>
   );
 };
@@ -14,6 +25,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#AF90D6',
+  },
+  upperview: {
+    flex: 0.4,
+    alignItems: 'center',
+    width: width,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 34,
+    position: 'relative',
+    bottom: 34,
+  },
+  initText: {
+    fontSize: 20,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  lowerview: {
+    flex: 1.6,
+    alignItems: 'center',
+    backgroundColor: '#221E3D',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    width: width,
+  },
+  img: {
+    position: 'relative',
+    bottom: 0,
+    opacity: 0.5,
   },
 });
 
