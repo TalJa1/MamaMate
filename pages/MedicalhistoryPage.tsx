8680b38940f2b9b6185d5cd62fa3cf75f9dd0d9f
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-06-23 11:55:29 +0700

    get image from local and render

--- pages/MedicalhistoryPage.tsx
+++ pages/MedicalhistoryPage.tsx
@@ -10,12 +10,15 @@ import {
 } from 'react-native';
 import React from 'react';
 import QuestionPageLayout from '../components/QuestionPageLayout';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const {width, height} = Dimensions.get('screen');
 
 const MedicalhistoryPage = () => {
   const [diseaseName, setDiseaseName] = React.useState<string>('');
+  const [image, setImage] = React.useState<Array<string>>([]);
+  console.log(image);
 
   const renderView = () => {
     return (
@@ -24,16 +27,36 @@ const MedicalhistoryPage = () => {
           <View style={styles.SectionStyle}>
             <TouchableOpacity
               style={styles.ImageStyle}
-              onPress={() => console.log('image')}>
-              <Image source={require('../assets/Icons/image.png')} />
+              onPress={async () => {
+                setImage([]);
+                const result: any = await launchImageLibrary({
+                  mediaType: 'photo',
+                  selectionLimit: 4,
+                });
+                for (let i = 0; i < result.assets.length; i++) {
+                  setImage(pre => {
+                    return [...pre, result.assets[i].uri];
+                  });
+                }
+              }}>
+              <Image
+                style={styles.ImageStyle}
+                source={require('../assets/Icons/image.png')}
+              />
             </TouchableOpacity>
             <TextInput
+              editable={false}
               placeholder="Tên bệnh"
               onChangeText={setDiseaseName}
               placeholderTextColor={'#E5CFEF61'}
               style={styles.textInput}
               value={diseaseName}
             />
+          </View>
+          <View style={styles.viewImg}>
+            {image?.map((v, i) => (
+              <Image key={i} style={styles.imgShow} source={{uri: v}} />
+            ))}
           </View>
           <TouchableOpacity style={styles.btnAdd}>
             <Text style={styles.inputTxtBtn}>Thêm</Text>
@@ -50,7 +73,7 @@ const MedicalhistoryPage = () => {
       title="Mẹ có tiền sử bệnh lý nào không ạ?"
       CustomView={renderView()}
       isDiscard={true}
-      value={5}
+      value={diseaseName === '' ? 0 : 5}
       nextPage="Medicalhistory"
     />
   );
@@ -90,10 +113,20 @@ const styles = StyleSheet.create({
   },
   ImageStyle: {
     position: 'absolute',
-    right: 10,
+    right: 8,
     height: 28,
     width: 28,
     zIndex: 1,
+  },
+  viewImg: {
+    marginTop: 20,
+    flexDirection: 'row',
+    columnGap: 10,
+  },
+  imgShow: {
+    height: 115,
+    width: 115,
+    borderRadius: 10,
   },
 });
 
