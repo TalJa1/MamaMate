4fdcbbdb6c4c9e1f1e9f5065902cd361c921e80c
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-07-01 13:47:15 +0700

    finish template for handbook detail

--- pages/Tracking/HandBookDetailPage.tsx
+++ pages/Tracking/HandBookDetailPage.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
 import {
+  Image,
   ImageBackground,
   ScrollView,
   StyleSheet,
@@ -15,26 +16,67 @@ import {renderHandBookTitle} from '../../services/renderData';
 import {backButtonWithoutArrowSVG} from '../../assets/svgXml';
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import givingBirthJourney from '../../data/handbook/givingBirthJourney.json';
+import {getHandBookImgDetail} from '../../services/imageHelper';
 // import useStatusBar from '../../services/customHook';
 
 type HandBookDetailRouteProp = RouteProp<RootStackParamList, 'HandBookDetail'>;
+interface Detail {
+  desTitle: string;
+  main: string;
+  content: string;
+}
+
+interface RenderDataItem {
+  img: number;
+  title: string;
+  smallDes: string;
+  detail: Detail;
+}
 
 const HandBookDetailPage = () => {
   const navigation = useNavigation<NativeStackNavigationProp<any>>();
-  // const [renderData, setRenderData] = React.useState([]);
+  const [renderData, setRenderData] = React.useState<RenderDataItem[]>([]);
   const route = useRoute<HandBookDetailRouteProp>();
   const {id} = route.params;
+
+  React.useEffect(() => {
+    switch (id) {
+      case 0:
+        setRenderData([]);
+        break;
+      case 1:
+        setRenderData([]);
+        break;
+      case 2:
+        setRenderData([]);
+        break;
+      case 3:
+        setRenderData([]);
+        break;
+      case 4:
+        setRenderData(givingBirthJourney);
+        break;
+      case 5:
+        setRenderData([]);
+        break;
+      case 6:
+        setRenderData([]);
+        break;
+      default:
+        break;
+    }
+  }, [id]);
 
   const handleNavigation = () => {
     navigation.goBack();
   };
-  // useStatusBar('transparent');
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       {headerRenderView(id, handleNavigation)}
-      {/* <View></View> */}
-    </ScrollView>
+      <ScrollView>{renderContent(id, renderData)}</ScrollView>
+    </View>
   );
 };
 
@@ -51,18 +93,32 @@ const headerRenderView = (id: number, navigate: () => void) => {
           <Text style={styles.headerTitle}>{renderHandBookTitle(id)}</Text>
         </View>
       </ImageBackground>
-      <View>
-        {/* {renderData.map((v, i: number) => (
-          {renderContent(i,)}
-        ))} */}
-      </View>
     </View>
   );
 };
 
-// const renderContent = (key: number, title: string, des: string, img: string) => {
-//   return <View></View>;
-// };
+const renderContent = (key: number, data: RenderDataItem[]) => {
+  return (
+    <View style={styles.renderCntScrollView}>
+      {data.map((v, i) => (
+        <TouchableOpacity key={i} style={styles.renderCntContainer}>
+          <View style={styles.renderCntImgContainer}>
+            <Image
+              style={styles.renderCntImg}
+              source={getHandBookImgDetail(v.img)}
+            />
+          </View>
+          <View style={styles.renderCntTxTContainer}>
+            <Text style={styles.renderCntTitle}>{v.title}</Text>
+            <Text style={styles.renderCntsmallDes} numberOfLines={2}>
+              {v.smallDes}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
 
 export default HandBookDetailPage;
 
@@ -96,5 +152,45 @@ const styles = StyleSheet.create({
   headerImgContainer: {
     width: vw(100),
     borderRadius: 90,
+  },
+  renderCntScrollView: {
+    width: vw(100),
+    rowGap: vh(2),
+    alignItems: 'center',
+    paddingVertical: vh(2),
+  },
+  renderCntContainer: {
+    height: 290,
+    width: vw(90),
+    backgroundColor: '#382E75',
+    paddingHorizontal: vw(4),
+    paddingTop: vh(1),
+    borderRadius: 10,
+  },
+  renderCntTxTContainer: {
+    paddingVertical: vh(2),
+    rowGap: vh(1),
+  },
+  renderCntTitle: {
+    textAlign: 'left',
+    fontWeight: '700',
+    fontSize: 16,
+    color: '#EAE1EE',
+  },
+  renderCntsmallDes: {
+    color: '#CDCDCD',
+    fontSize: 12,
+    fontWeight: '400',
+  },
+  renderCntImgContainer: {
+    width: 'auto',
+    height: 160,
+    borderRadius: 16,
+    overflow: 'hidden',
+  },
+  renderCntImg: {
+    width: '100%',
+    flex: 1,
+    resizeMode: 'cover',
   },
 });
